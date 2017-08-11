aa87f52786ecd21b9a8e94c245b68b3af33fe9d4
Rikka <rikka@xing.moe>
2017-08-11 14:42:46 +0800

    feat: add about

--- app/src/main/java/moe/shizuku/fcmformojo/MainActivity.java
+++ app/src/main/java/moe/shizuku/fcmformojo/MainActivity.java
@@ -1,7 +1,16 @@
 package moe.shizuku.fcmformojo;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends FragmentActivity {
 
@@ -14,5 +23,26 @@ public class MainActivity extends FragmentActivity {
                     .replace(android.R.id.content, new SettingsFragment())
                     .commit();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onMenuItemSelected(int featureId, MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_about:
+                Dialog dialog = new AlertDialog.Builder(this)
+                        .setView(R.layout.dialog_about)
+                        .show();
+                ((TextView) dialog.findViewById(R.id.icon_credits)).setMovementMethod(LinkMovementMethod.getInstance());
+                ((TextView) dialog.findViewById(R.id.icon_credits)).setText(Html.fromHtml(getString(R.string.about_icon_credits)));
+
+                break;
+        }
+        return super.onMenuItemSelected(featureId, item);
     }
 }
