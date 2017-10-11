7c9d56d7caa48d881aa7cb90b776d2c1870ffb52
RikkaW <rikka@shizuku.moe>
2017-10-11 19:52:56 +0800

    feat: introduce Font Provider

--- app/src/main/java/moe/shizuku/fcmformojo/BaseActivity.java
+++ app/src/main/java/moe/shizuku/fcmformojo/BaseActivity.java
@@ -1,13 +1,44 @@
 package moe.shizuku.fcmformojo;
 
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 import android.view.MenuItem;
+
+import moe.shizuku.fontprovider.FontProviderClient;
+import moe.shizuku.fontprovider.FontRequest;
+import moe.shizuku.fontprovider.FontRequests;
 
 /**
  * Created by rikka on 2017/8/16.
  */
 
 public class BaseActivity extends FragmentActivity {
+
+    private static boolean sFontProviderInitialized = false;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (!sFontProviderInitialized) {
+            // 替换默认 emoji 字体
+            FontRequests.setDefaultSansSerifFonts(FontRequest.DEFAULT, FontRequest.NOTO_COLOR_EMOJI);
+
+            // 创建 FontProviderClient
+            FontProviderClient.create(this, new FontProviderClient.Callback() {
+                @Override
+                public boolean onServiceConnected(FontProviderClient client, ServiceConnection serviceConnection) {
+                    client.replace("sans-serif", "Noto Sans CJK");
+                    client.replace("sans-serif-medium", "Noto Sans CJK");
+                    return true;
+                }
+            });
+
+            sFontProviderInitialized = true;
+        }
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
